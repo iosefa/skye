c8b4601e8d40752032ee72244e00f2471aa9a2d5
Iosefa Percival <ipercival@gmail.com>
2023-04-25 12:45:49 +0900

    bump version to 0.0.5
    Signed-off-by: Iosefa Percival <ipercival@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="skye",
-    version="0.0.4",
+    version="0.0.5",
     author="Iosefa Percival",
     author_email="ipercival@gmail.com",
     description="360-degree image analysis for forest ecology",
